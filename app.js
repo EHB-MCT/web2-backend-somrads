3453831589222f01821451a2baeac119680002c1
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-12-14 16:51:07 +0100

    config json file

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 //express
 const express = require('express')();
 const app = express;
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+//mongo config
 
 
-//middleware
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-const cors = require('cors');
-app.use(cors);
+// //middleware
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// const cors = require('cors');
+// app.use(cors);
 
  //Root route
 app.get('/', (req, res) => {
-    // res.sendFile(__dirname + '/public/info.html');
-  res.send('Hello World!')
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+
+app.get("/people", async (req, res) => {
+
+  try {
+    //Read file
+
+
+
+    //Send file
+    
+  } catch (error) {
+    
+  }
+
 })
 
 app.listen(port, () => {
   console.log(`REST API is running at http://localhost:${port}`);
-})
+}) 
