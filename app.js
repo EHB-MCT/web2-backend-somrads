fba04fa6f0a7d177e2f3ffb0cff10e5da8bd8a30
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-12-14 17:29:56 +0100

    get route

--- app.js
+++ app.js
@@ -3,32 +3,50 @@ const express = require('express')();
 const app = express;
 const port = 3000;
 
-//mongo config
-
-
 // //middleware
 // const bodyParser = require('body-parser');
 // app.use(bodyParser.json());
 // const cors = require('cors');
 // app.use(cors);
 
+//mongo config
+const {
+  MongoClient,
+  ObjectId
+} = require('mongodb');
+const config = require('./config.json')
+//new mongo client
+const client = new MongoClient(config.baseUrl);
+
  //Root route
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
-
-app.get("/people", async (req, res) => {
-
+//APP ROUTES
+app
+.get("/people", async (req, res) => {
   try {
-    //Read file
+    //Read file//
 
+     //connect db
+     await client.connect();
+     console.log("Connected correctly to database");
 
+     //use the collection
+     const coll = client.db('StarWarsDb').collection('peoples');
+     const people = await coll.find({}).toArray();
 
-    //Send file
+      //send back the file
+      res.status(200).send(people)
+
     
   } catch (error) {
-    
+    console.log(error);
+    res.status(500).send({
+      error: "GET route error ",
+      value: error
+    })
   }
 
 })
