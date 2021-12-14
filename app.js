95b504461405477818e5500ed663298fab9b9b70
Somrad Sharma <somrad.sharma@student.ehb.be>
2021-12-14 17:35:06 +0100

    get by id

--- app.js
+++ app.js
@@ -51,6 +51,41 @@ app
 
 })
 
+//GET all by :ID from db
+.get('/people/:id', async (req, res) => {
+
+  //id is located in the query: req.params.id
+  try {
+    //connect db
+    await client.connect();
+
+    //retrieve challeng data
+    const coll = client.db('StarWarsDb').collection('peoples')
+
+    //only look for a challenge with id
+    const query = {
+      _id: ObjectId(req.params.id)
+    };
+
+    const people = await coll.findOne(query)
+
+    if (people) {
+      //send back the file
+      res.status(200).send(people);
+      return;
+    } else {
+      res.status(400).send("request could not be found with id " + req.params.id)
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "GET by id route error",
+      value: error
+    })
+  }
+})
+
 app.listen(port, () => {
   console.log(`REST API is running at http://localhost:${port}`);
 }) 
